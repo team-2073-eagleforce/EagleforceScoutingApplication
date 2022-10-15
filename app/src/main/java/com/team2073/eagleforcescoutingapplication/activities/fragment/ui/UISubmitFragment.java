971d1edcb90ce36633e81d4e74538265c1a9c2ce
eagle9518 <j007wo2@gmail.com>
2022-10-14 23:43:34 -0700

    Replaces commas with semicolons in comments

--- app/src/main/java/com/team2073/eagleforcescoutingapplication/activities/fragment/ui/UISubmitFragment.java
+++ app/src/main/java/com/team2073/eagleforcescoutingapplication/activities/fragment/ui/UISubmitFragment.java
@@ -181,7 +181,8 @@ public class UISubmitFragment extends Fragment implements View.OnClickListener {
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.uiSubmitButton:
-                scoutingFormPresenter.saveData("comments", formComments.getText().toString());
+                String comments = formComments.getText().toString().replace(',', ';');
+                scoutingFormPresenter.saveData("comments", comments);
                 if (scoutingFormPresenter.readData("comments").length() == 0 || scoutingFormPresenter.readData("name").length() == 0){
                     Toast.makeText(this.getActivity(), "Some Fields are Empty", Toast.LENGTH_SHORT).show();
                 } else {
